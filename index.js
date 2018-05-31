b9ac2d99d1eea49d7cf8f780ab904624454556cc
Gabriel Valle <gvalle91@gmail.com>
2018-05-31 17:20:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+function increaseRankBy(n){
+  var array = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i <array.length; i++){
+    array[i].innerHTML = (parseInt(array[i].innerHTML) + n).toString();
+  }
+}
+function deepestChild(){
+  var list = document.getElementById('grand-node').querySelectorAll('div');
+  return list[list.length-1];
+}
