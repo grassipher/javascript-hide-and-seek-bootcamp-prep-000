247b42bf6a33b30f8d1f29f5e234360a22a93962
Gabriel Valle <gvalle91@gmail.com>
2018-05-31 17:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function nestedTarget(){
   return document.getElementById('nested').querySelector('.target');
 }
 function increaseRankBy(n){
-  return document.getElementById('nested').querySelectorAll('.ranked-list');
   var array = document.getElementById('nested').querySelectorAll('.ranked-list');
   for (var i = 0; i <array.length; i++){
     array[i].innerHTML = parseInt(array[i].innerHTML)+parseInt(n);
